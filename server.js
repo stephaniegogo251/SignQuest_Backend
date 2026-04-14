54ce9dc452b5a46cf8afd25005e6ed6fe1fc62ee
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2026-04-14 21:46:11 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,135 @@
+var express = require("express");
+var app = express(); //create instance of express.js
+var path = require("path");
+var fs = require("fs");
+var dotenv = require('dotenv');
+const port = process.env.PORT || 3000;
+dotenv.config();
+const cors = require('cors');
+
+//config express.js
+//cors middleware - applied to all requests to allow resources to be shared across diff domains
+app.use ((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+ 
+    next();
+});
+app.use(cors());
+
+//logging middleware
+app.use(function(req, res, next){
+    //logs request method, path, query and timestamp
+    console.log(`${req.method} ${req.path} ${JSON.stringify(req.query)} time: ${new Date()}`);
+    next();
+});
+ 
+//built-in express middleware to parse incoming json requests
+app.use(express.json());
+
+//middleware to serve static files (image files in lessons)
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+//middleware to handle and send static files
+app.use(function(req, res, next){
+    var filePath = path.join(__dirname, "static", req.url);
+    fs.stat(filePath, function(err, fileInfo){
+        if (err){
+            next();
+            return;
+        }
+        if(fileInfo.isFile())
+            res.sendFile(filePath);
+        else
+            next();
+    });
+});
+
+const MongoClient = require('mongodb').MongoClient; //import mongoclient(object part of mongodb driver) to talk to mongo server
+const uri = process.env.MONGODB_URI; //mongodb connection uri
+
+let db;
+//connect to mongodb
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+    db = client.db('SignQuest')}); //connect to specific database
+
+//default route for root path
+app.get('/', (req, res, next) => {
+    res.send('Select a collection, e.g., /collection/messages');
+});
+
+//route paramter middleware to attach collection to the request object for all other request handlers
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+//get route to return lessons collection as an array
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
+        res.send(results);
+    }); 
+});
+
+//post route to add order to orders collection
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e);
+        res.send(results.ops);
+    });
+});
+
+//get route to search for specific lessons from collection
+app.get('/collection/:collectionName/search', (req, res, next) => {
+    const query = req.query.q;
+    const searchPattern = new RegExp(query, 'i'); //case-insensitive regex pattern
+    let searchQuery = {
+        $or: [
+            {name: {$regex: searchPattern}},
+            {location: {$regex: searchPattern}},
+        ]
+    };
+
+    let numQuery = 0;
+    if (!isNaN(parseInt(query))) {
+        numQuery = parseInt(query);
+        searchQuery = {
+        $or: [
+            {name: {$regex: searchPattern}},
+            {location: {$regex: searchPattern}},
+            {price: numQuery},
+            {availableSeats:numQuery}
+        ]
+    };
+    }
+
+    req.collection.find(searchQuery).toArray((e, results) => {
+        if (e) return next(e);
+        res.send(results);
+    }); 
+
+});
+
+const ObjectID = require('mongodb').ObjectID;
+
+//put route to update available spaces of a lesson
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.updateOne(
+        {_id: new ObjectID(req.params.id)},
+        {$set: req.body},
+        {safe: true, multi: false},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'});
+        });
+});
+
+//server starts and listens on port 3000
+app.listen(port, function(){
+    console.log("App started on port 3000");
+    console.log("http://localhost:3000")
+});
