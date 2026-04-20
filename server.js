77c1d2e47d47057b9c882fbdadfe85ce392655aa
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2026-04-20 10:12:55 +0400

    diff cors

--- server.js
+++ server.js
@@ -9,15 +9,19 @@ const cors = require('cors');
 
 //config express.js
 //cors middleware - applied to all requests to allow resources to be shared across diff domains
-app.use ((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
- 
-    next();
-});
-app.use(cors());
+// app.use ((req,res,next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+//     next();
+// });
+app.use(cors({
+    origin: '*',
+    methods: ['POST', 'GET', 'OPTIONS'],
+    allowedHeaders: ['Content-Type']
+}));
 
 //built-in express middleware to parse incoming json requests
 app.use(express.json({ limit: '10mb'}));
